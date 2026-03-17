04907fb6cbc2b00202696383a72e1d80b9a64a68
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 04:58:03 +0530

    feat: integrate config/config.py

---
+++ config/config.py
@@ -0,0 +1,7 @@
+import os
+
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_USERNAME = os.environ.get('DB_USERNAME')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_NAME = os.environ.get('DB_NAME')
